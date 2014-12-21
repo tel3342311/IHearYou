563b4b7bc15fb09791a208ce13393ee36ba9081d
Allen Sun <allen.sun@acer.com>
2014-12-22 00:49:55 +0800

    Add lesson search result page

--- app/src/main/java/com/ihy/ihearyou/activity/LessonActivity.java
+++ app/src/main/java/com/ihy/ihearyou/activity/LessonActivity.java
@@ -2,29 +2,88 @@ package com.ihy.ihearyou.activity;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.ihy.ihearyou.R;
 import com.ihy.ihearyou.fragment.LessonContentFragment;
 import com.ihy.ihearyou.fragment.LessonIntroductionFragment;
 import com.ihy.ihearyou.fragment.LessonMainFragment;
+import com.ihy.ihearyou.fragment.LessonSearchResult;
 
 public class LessonActivity extends ActionBarActivity implements
         LessonMainFragment.OnFragmentInteractionListener,
         LessonIntroductionFragment.OnFragmentInteractionListener,
-        LessonContentFragment.OnFragmentInteractionListener {
+        LessonContentFragment.OnFragmentInteractionListener,
+        LessonSearchResult.OnFragmentInteractionListener {
 
     LessonMainFragment mMainFrag;
     LessonIntroductionFragment mIntroductionFrag;
     LessonContentFragment mContentFragment;
+    LessonSearchResult mSearchResult;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson);
         switchToMainView();
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            searchKeyWord(query);
+        }
+    }
+
+    private void searchKeyWord(String query) {
+        if (query.equals("goat")) {
+            switchToSearchView();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_lesson, menu);
+
+        //setup search
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(true);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void openSearch() {
+
     }
 
     private void switchToMainView() {
@@ -43,12 +102,19 @@ public class LessonActivity extends ActionBarActivity implements
         fragmentTransaction.commit();
     }
 
-
     private void switchToContentView() {
         mContentFragment = LessonContentFragment.newInstance("","");
         FragmentManager fm = this.getFragmentManager();
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
         fragmentTransaction.replace(R.id.lesson_frame, mContentFragment);
+        fragmentTransaction.commit();
+    }
+
+    private void switchToSearchView() {
+        mSearchResult = LessonSearchResult.newInstance("","");
+        FragmentManager fm = this.getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.lesson_frame, mSearchResult);
         fragmentTransaction.commit();
     }
 
