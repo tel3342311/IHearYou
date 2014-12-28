d988618240c6ef3be5ec3568315c10550124815e
Allen Sun <allen.sun@acer.com>
2014-12-29 02:33:06 +0800

    Add conversation UI first tag

--- app/src/main/java/com/ihy/ihearyou/activity/ConversationActivity.java
+++ app/src/main/java/com/ihy/ihearyou/activity/ConversationActivity.java
@@ -1,7 +1,135 @@
 package com.ihy.ihearyou.activity;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
+import android.view.animation.TranslateAnimation;
+import android.widget.TextView;
 
-public class ConversationActivity extends ActionBarActivity {
+import com.ihy.ihearyou.R;
+import com.ihy.ihearyou.fragment.InterperterFragment;
+import com.ihy.ihearyou.fragment.PhoneCallFragment;
+import com.ihy.ihearyou.fragment.VideoRecordFragment;
+
+public class ConversationActivity extends ActionBarActivity implements
+        android.app.ActionBar.TabListener,
+        ViewPager.OnPageChangeListener,
+        VideoRecordFragment.OnFragmentInteractionListener,
+        InterperterFragment.OnFragmentInteractionListener,
+        PhoneCallFragment.OnFragmentInteractionListener{
+    private ViewPager mViewPager;
+    private int mCurrentPosition;
+    private final static int FRAGMENT_COUNT = 3;
+    private InterperterFragment mInterperterFragment;
+    private VideoRecordFragment mVideoRecordFragment;
+    private PhoneCallFragment mPhoneCallFragment;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversation);
+        findViews();
+    }
+
+    private void findViews() {
+
+        mInterperterFragment = InterperterFragment.newInstance("","");
+        mVideoRecordFragment = VideoRecordFragment.newInstance("","");
+        mPhoneCallFragment = PhoneCallFragment.newInstance("","");
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(new HomeViewPagerAdapter(getSupportFragmentManager()));
+        mViewPager.setOnPageChangeListener(this);
+    }
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+        mCurrentPosition = position;
+        TextView interpreter = (TextView)findViewById(R.id.text_interpreter);
+        TextView video_record = (TextView)findViewById(R.id.text_video_record);
+        TextView phone_call = (TextView)findViewById(R.id.text_phone);
+
+        switch (position) {
+            case 0:
+                interpreter.setTextColor(getResources().getColor(android.R.color.white));
+                video_record.setTextColor(getResources().getColor(android.R.color.darker_gray));
+                phone_call.setTextColor(getResources().getColor(android.R.color.darker_gray));
+                break;
+            case 1:
+                interpreter.setTextColor(getResources().getColor(android.R.color.darker_gray));
+                video_record.setTextColor(getResources().getColor(android.R.color.white));
+                phone_call.setTextColor(getResources().getColor(android.R.color.darker_gray));
+                break;
+            case 2:
+                interpreter.setTextColor(getResources().getColor(android.R.color.darker_gray));
+                video_record.setTextColor(getResources().getColor(android.R.color.darker_gray));
+                phone_call.setTextColor(getResources().getColor(android.R.color.white));
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    public class HomeViewPagerAdapter extends FragmentPagerAdapter {
+
+        public HomeViewPagerAdapter(FragmentManager fm) {
+
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return mInterperterFragment;
+                case 1:
+                    return mVideoRecordFragment;
+                case 2:
+                    return mPhoneCallFragment;
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return FRAGMENT_COUNT;
+        }
+    }
 }
---
+++ app/src/main/java/com/ihy/ihearyou/fragment/VideoRecordFragment.java
@@ -0,0 +1,85 @@
+package com.ihy.ihearyou.fragment;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ihy.ihearyou.R;
+
+public class VideoRecordFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    // TODO: Rename and change types and number of parameters
+    public static VideoRecordFragment newInstance(String param1, String param2) {
+        VideoRecordFragment fragment = new VideoRecordFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public VideoRecordFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_video_record, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+}
