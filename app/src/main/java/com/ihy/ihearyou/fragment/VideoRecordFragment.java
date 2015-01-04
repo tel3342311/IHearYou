223fb9ed96a85f5c5bf4658eb85b2b8fbf4413bf
Allen Sun <allen.sun@acer.com>
2015-01-05 02:47:02 +0800

    Add video preview UI

--- app/src/main/java/com/ihy/ihearyou/fragment/VideoRecordFragment.java
+++ app/src/main/java/com/ihy/ihearyou/fragment/VideoRecordFragment.java
@@ -1,15 +1,20 @@
 package com.ihy.ihearyou.fragment;
 
 import android.app.Activity;
+import android.content.res.Configuration;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
 
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.ihy.ihearyou.R;
+import com.ihy.ihearyou.component.CameraPreview;
 
 public class VideoRecordFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
@@ -22,6 +27,10 @@ public class VideoRecordFragment extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+    private static Camera mCamera;
+    private CameraPreview mCameraPreview;
+    private View mRootView;
+    private View mVideoView;
 
     // TODO: Rename and change types and number of parameters
     public static VideoRecordFragment newInstance(String param1, String param2) {
@@ -50,7 +59,10 @@ public class VideoRecordFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_video_record, container, false);
+        mRootView = inflater.inflate(R.layout.fragment_video_record, container, false);
+        mVideoView = mRootView.findViewById(R.id.video_frame);
+        getCameraInstance();
+        return mRootView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -82,4 +94,52 @@ public class VideoRecordFragment extends Fragment {
         public void onFragmentInteraction(Uri uri);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        setCameraDisplayOrientation(getActivity(), Camera.CameraInfo.CAMERA_FACING_BACK, mCamera);
+        mCameraPreview = new CameraPreview(getActivity(), mCamera);
+        ((FrameLayout)mVideoView).addView(mCameraPreview, 0);
+    }
+
+    public static Camera getCameraInstance(){
+        try {
+            mCamera = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return mCamera; // returns null if camera is unavailable
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        setCameraDisplayOrientation(getActivity(), Camera.CameraInfo.CAMERA_FACING_BACK, mCamera);
+    }
+
+    public static void setCameraDisplayOrientation(Activity activity,
+                                                   int cameraId, android.hardware.Camera camera) {
+        android.hardware.Camera.CameraInfo info =
+                new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(cameraId, info);
+        int rotation = activity.getWindowManager().getDefaultDisplay()
+                .getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0: degrees = 0; break;
+            case Surface.ROTATION_90: degrees = 90; break;
+            case Surface.ROTATION_180: degrees = 180; break;
+            case Surface.ROTATION_270: degrees = 270; break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        camera.setDisplayOrientation(result);
+    }
 }
